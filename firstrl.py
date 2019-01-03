56061544bd7ea0932e1ca55ad000dcf0b01dbfc1
Walkowiak <ewalkowiak@datacenter.radiangroupinc.net>
2019-01-03 10:33:41 -0500

    intial code clean up

--- firstrl.py
+++ firstrl.py
@@ -2,59 +2,54 @@
 
 import tcod
 
-# ######################################################################
+# ----------------------------------------------------------------------
 # Global Game Settings
-# ######################################################################
+# ----------------------------------------------------------------------
 
-# Windows Controls
-FULLSCREEN = False
-SCREEN_WIDTH = 80  # characters wide
-SCREEN_HEIGHT = 50  # characters tall
-LIMIT_FPS = 20  # 20 frames-per-second maximum
-# Game Controls
-TURN_BASED = True  # turn-based game
+# Window Settings
+FULLSCREEN = False  # Fullscreen Control
+SCREEN_WIDTH = 80   # Characters Wide
+SCREEN_HEIGHT = 50  # Characters Tall
+LIMIT_FPS = 20      # 20 FPS MAX
+
+# Game Control Method
+TURN_BASED = True  # Turn-Based Boolean
 
 def initialize_game():
-    # Setup player
-    global player_x, player_y
-    player_x = SCREEN_WIDTH // 2
-    player_y = SCREEN_HEIGHT // 2
-    # Setup Font
-    font_filename = 'arial10x10.png'
+    # Initialize Player
+    global player_x, player_y # Player Position
+    player_x = SCREEN_WIDTH // 2    
+    player_y = SCREEN_HEIGHT // 2   
+    # Initialize Font
+    font_filename = 'arial10x10.png' # Font
     tcod.console_set_custom_font(font_filename, tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
-
-    # Initialize screen
-    title = 'Python 3 + Libtcod tutorial'
+    # Initialize Window
+    title = 'asciiroguelike' # Window Title
     tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, FULLSCREEN)
-
-    # Set FPS
+    # Initialize FPS
     tcod.sys_set_fps(LIMIT_FPS)
 
-# ######################################################################
-# User Input
-# ######################################################################
+# ----------------------------------------------------------------------
+# User Input / Controls
+# ----------------------------------------------------------------------
 
-def get_key_event(turn_based = None):
-    if turn_based:
-        # Turn-based game play; wait for a key stroke
-        key = tcod.console_wait_for_keypress(True)
-    else:
-        # Real-time game play; don't wait for a player's key stroke
-        key = tcod.console_check_for_keypress()
+def get_key_event(turn_based = None):               # Gather Keyboard Input
+    if turn_based:  
+        key = tcod.console_wait_for_keypress(True)  # Turn-Based: Wait For Input
+    else:           
+        key = tcod.console_check_for_keypress()     # Real-Time: Don't Wait
     return key
 
 def handle_keys():
-    global player_x, player_y
-
-    key = get_key_event(TURN_BASED)
-
+    global player_x, player_y           # Player Position
+    key = get_key_event(TURN_BASED)     # Gather Keyboard Input
+    # Key Check For Fullscreen / Exiting
     if key.vk == tcod.KEY_ENTER and key.lalt:
-        # Alt+Enter: toggle fullscreen
-        tcod.console_set_fullscreen(not tcod.console_is_fullscreen())
+        tcod.console_set_fullscreen(not tcod.console_is_fullscreen())   # Alt + Enter: Toggle Fullscreen
     elif key.vk == tcod.KEY_ESCAPE:
-        return True  # exit game
-
-    # movement keys
+        return True  # Exit Game
+    #--------------------------------------------------   
+    # Movement Keys
     if tcod.console_is_key_pressed(tcod.KEY_UP):
         player_y -= 1
     elif tcod.console_is_key_pressed(tcod.KEY_DOWN):
@@ -63,22 +58,22 @@ def handle_keys():
         player_x -= 1
     elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
         player_x += 1
+    #-------------------------------------------------- 
 
-
-#############################################
+# ----------------------------------------------------------------------
 # Main Game Loop
-#############################################
+# ----------------------------------------------------------------------
 
 def main():
-    initialize_game()
-
-    exit_game = False
+    initialize_game()   # Run Game
+    exit_game = False   # Keep Open
     while not tcod.console_is_window_closed() and not exit_game:
+        #-----------------------------------------------------------------
+        # Player:
         tcod.console_set_default_foreground(0, tcod.white)
-        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)
+        tcod.console_put_char(0, player_x, player_y, '@', tcod.BKGND_NONE)  # Player Character
         tcod.console_flush()
-        tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE)
-
-        exit_game = handle_keys()
-
+        tcod.console_put_char(0, player_x, player_y, ' ', tcod.BKGND_NONE)  # Previous Player Location
+        #-----------------------------------------------------------------
+        exit_game = handle_keys()   # Exit
 main()
